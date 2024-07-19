f2378d3f5be06fedb8ed138113c820b71eae7a70
chirag2103 <chirag21032004@gmail.com>
2024-07-19 07:41:40 +0530

    ADDED SPLINE

--- src/App.js
+++ src/App.js
@@ -7,6 +7,13 @@ function App() {
   return (
     <Router>
       <Navbar />
+      <iframe
+        src='https://my.spline.design/visualicons-4120e9c86553fc08853f314d8c8bc94d/'
+        frameBorder='0'
+        width='100%'
+        height='500'
+        className='fixed top-0 left-0 w-full h-full -z-10'
+      ></iframe>
       <Routes>
         <Route exact path='/admin' Component={Navbar} />
       </Routes>
